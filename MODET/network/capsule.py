854e46dee08b6a471d1871b20cf4330aea4d6581
zbuster05 <zakary.saya@gmail.com>
2020-11-13 10:09:04 -0800

    Deleted commented code

--- MODET/network/capsule.py
+++ MODET/network/capsule.py
@@ -50,30 +50,3 @@ class ClassCapsLayer(nn.Module):
 
         return outputVj
 
-
-### Sorry @quantumish. Already implimented active routing... See above.
-### We did steal some of your stuff
-
-# class Capsule(nn.Module):
-    # def __init__(self, dimensions):
-        # self.dims = dimensions
-        # self.children = []
-
-    # # We need to initialize connections/weights after all capsules defined
-    # def init_connection(self, capsule):
-# #        self.weights = [] This will be a problem...
-        # self.children.append(capsule)
-    
-    # # Boilerplate-y for now.
-    # def forward(self):
-        # self.s = torch.tensor([0.0]*self.dims) # TODO understanding dimensionalities and specifics of calcs will take a bit
-        # for child in self.children:
-            # # Begin with affine transform of output
-            # # child.weights[self] is sketch, trying to indicate $\hat{\vec{u}}_{j|i} = \vec{W}_{ij} \vec{u}_i$
-            # # Note that there is no normal u, just the child capsule's s because prev layers output is next layer input
-            # uhat = child.weights[self] * child.s
-            # # Compute the traditional Wx+b step, in this case $\sum_i c_ij \hat{\vec{u}}_{j|i}$
-            # self.s += self.route_consts[child] * uhat
-        # # Squashing function
-        
-

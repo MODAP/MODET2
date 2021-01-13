e71b2fd19d4edd814ea839e6a3f36df17ce81525
Houjun Liu <hliu@shabang.cf>
2021-01-13 14:49:12 -0800

    NW: it's probably broken

--- scratchpad.py
+++ scratchpad.py
@@ -1,11 +1,13 @@
+import torch
+
 from MODET.data import preprocessing
 from MODET.network import network
 
 
-capsNet = network.CapsNet(5, 10)
+capsNet = network.CapsNet(3, 10)
 
 
 # ds = preprocessing.Dataset("./export-2020-10-30T16_41_24.239Z.csv")
+david = torch.rand((64, 3, 32, 32))
 
-breakpoint()
-
+print(capsNet(david))
